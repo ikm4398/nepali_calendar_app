dcc80d1f7bd7fab4847c1c4c8b4262a815a5a19b
Indra <ikm4398@gmail.com>
2025-05-14 15:25:19 +0545

    delete doctype

--- nepali_calendar/nepali_calendar/doctype/nepali_date/nepali_date.js
+++
@@ -1,5 +0,0 @@
-// nepali_calendar/nepali_calendar/doctype/nepali_date/nepali_date.js
-
-frappe.ui.form.on("Nepali_Date", {
-	// Remove or comment out the onload handler
-});
--- nepali_calendar/nepali_calendar/doctype/nepali_date/nepali_date.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, indra @deskgoo and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class Nepali_Date(Document):
-	pass
